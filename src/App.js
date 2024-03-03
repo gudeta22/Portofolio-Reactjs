8dda0489f740d4faa23f9415f3615fa67a21b831
Gudeta Buli <gbteshite6195@gmail.com>
2024-03-03 09:51:49 +0300

    title logo changed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+// import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
